16afe79a3d17702128a46e8464edb41c22367410
bubblegum-pop <201892308+bubblegum-pop@users.noreply.github.com>
2025-03-31 13:41:24 -0700

    Complete exercise 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,29 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let isLeapYear;
 
+    const isDivisibleBy = function(dividend, divisor) {
+        if (dividend % divisor === 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    const isDivisibleBy4 = isDivisibleBy(year, 4);
+    const isDivisibleBy100 = isDivisibleBy(year, 100);
+    const isDivisibleBy400 = isDivisibleBy(year, 400);
+
+    if (isDivisibleBy400) {
+        isLeapYear = true;
+    } else if (isDivisibleBy100) {
+        isLeapYear = false;
+    } else if (isDivisibleBy4) {
+        isLeapYear = true;
+    } else {
+        isLeapYear = false;
+    }
+    
+    return isLeapYear;
 };
 
 // Do not edit below this line
