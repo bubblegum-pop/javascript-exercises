0c511d33e5ef992153471325091313e78da58267
bubblegum-pop <201892308+bubblegum-pop@users.noreply.github.com>
2025-03-31 10:25:46 -0700

    Complete exercise 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const lastIndex = string.length - 1;
+    let result = "";
+    
+    for (let i = lastIndex; i >= 0; i--) {
+        result += string.at(i);
+    }
 
+    return result;
 };
+
+console.log(reverseString("batman"));
 
 // Do not edit below this line
 module.exports = reverseString;
