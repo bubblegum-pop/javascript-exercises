a422e433a5e98d14983e3e5265e34a516bed121a
bubblegum-pop <201892308+bubblegum-pop@users.noreply.github.com>
2025-03-31 12:01:58 -0700

    Write exericse 05_sumAll pseudocode

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(arg1, arg2) {
+    
 };
+
+/*
+Determine if arguments are positive integers
+If they are, determine which argument is smallest
+Start with the smallest argument, and increment by 1 until reaching the largest
+Add each incremented value to result
+Return result
+If at least one argument is not a positive integer, result = "ERROR"
+
+To check for positive integer:
+- Is it a number?
+- Is it a positive number?
+- Is it an integer? 2 % 1 = 0
+*/
+
 
 // Do not edit below this line
 module.exports = sumAll;
