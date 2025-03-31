e0ee79d2466c41669d7def117ff340f4b66508e1
bubblegum-pop <201892308+bubblegum-pop@users.noreply.github.com>
2025-03-31 14:11:28 -0700

    Complete exercise 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  const degreesF = num;
+  const degreesC = (degreesF - 32) * (5 / 9);
+
+  let result = Math.round(degreesC * 10) / 10.0;
+
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  const degreesC = num;
+  const degreesF = degreesC * (9 / 5) + 32;
+
+  let result = Math.round(degreesF * 10) / 10.0;
+
+  return result;
 };
 
 // Do not edit below this line
