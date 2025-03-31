94a3886d465c5bb91b3e049a0a177201db96f721
bubblegum-pop <201892308+bubblegum-pop@users.noreply.github.com>
2025-03-31 13:11:42 -0700

    Complete exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,21 +1,37 @@
 const sumAll = function(arg1, arg2) {
-    
+    let sum = "ERROR";
+
+    // Returns true if the argument is a positive integer
+    const isPosInt = function(arg) {
+        let result = false;
+            
+        if (typeof arg === "number") {
+            if (arg > 0) {
+                if (arg % 1 === 0) {
+                    result = true;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    // If both arguments are positive integers, sum all values in range
+    if (isPosInt(arg1) === true) {
+        if (isPosInt(arg2) === true) {
+            let numberToAdd = Math.min(arg1, arg2);
+            const endValue = Math.max(arg1, arg2);
+            sum = 0;
+
+            while (numberToAdd <= endValue) {
+                sum += numberToAdd;
+                numberToAdd++;
+            }
+        }
+    }
+
+    return sum;
 };
-
-/*
-Determine if arguments are positive integers
-If they are, determine which argument is smallest
-Start with the smallest argument, and increment by 1 until reaching the largest
-Add each incremented value to result
-Return result
-If at least one argument is not a positive integer, result = "ERROR"
-
-To check for positive integer:
-- Is it a number?
-- Is it a positive number?
-- Is it an integer? 2 % 1 = 0
-*/
-
 
 // Do not edit below this line
 module.exports = sumAll;
