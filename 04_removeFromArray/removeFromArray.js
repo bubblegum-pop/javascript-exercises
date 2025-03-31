51815104ed92e92fb2a029f50c41ead38c43cc80
bubblegum-pop <201892308+bubblegum-pop@users.noreply.github.com>
2025-03-31 11:31:06 -0700

    Complete exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...valuesToRemove) {
+    const result = [];
+
+    for (const item of array) {
+        let remove = false;
+        for (const value of valuesToRemove) {
+            if (item === value) {
+                remove = true;
+            }
+        }
+        if (remove === false) {
+            result.push(item);
+        }
+    }
+
+    return result;
 };
 
 // Do not edit below this line
