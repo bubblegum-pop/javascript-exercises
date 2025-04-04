7ba91118cbc6eb97ff1225ccb52a864bd077a9ec
bubblegum-pop <201892308+bubblegum-pop@users.noreply.github.com>
2025-04-03 21:45:19 -0700

    Complete exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let initStr = str.replace(/[^\p{L}\p{N}]/gu, "").toLowerCase();
+  let reverse = initStr.slice().split("").reverse().join("");
+  return initStr == reverse;
 };
 
 // Do not edit below this line
