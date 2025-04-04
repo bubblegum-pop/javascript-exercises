1a99200538bef5ab86909d22f22a8cc5d6fe01c2
bubblegum-pop <201892308+bubblegum-pop@users.noreply.github.com>
2025-04-04 08:38:34 -0700

    Complete exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+  const member = parseInt(num);
+  let num1 = 0;
+  let num2 = 1;
+  let sum;
+  if (member < 0) {
+    return "OOPS";
+  } else if (member === 0) {
+    return num1;
+  } else if (member === 1) {
+    return num2;
+  } else {
+    for (let i = 1; i < num; i++) {
+      sum = num1 + num2;
+      num1 = num2;
+      num2 = sum;
+    }
+    return sum;
+  }
 };
 
 // Do not edit below this line
