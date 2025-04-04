00f99fdd90906e8528d5b5bf864de23bc3c284be
bubblegum-pop <201892308+bubblegum-pop@users.noreply.github.com>
2025-04-04 09:15:34 -0700

    Complete exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(objArr) {
+  let ageOfOldest = 0;
+  let indexOfOldest;
+  
+  objArr.forEach((person, index) => {
+    let endYear;
+    if ("yearOfDeath" in person) {
+      endYear = person["yearOfDeath"];
+    } else {
+      endYear = new Date().getFullYear();
+    }
+    let age = endYear - person["yearOfBirth"];
+    if (age > ageOfOldest) {
+      ageOfOldest = age;
+      indexOfOldest = index;
+    }
+  });
 
+  return objArr.at(indexOfOldest);
 };
 
 // Do not edit below this line
